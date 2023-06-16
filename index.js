eccf61f8546d58574d4aacc3a379160ce8cd6b76
Hans Bulder <hans@vetdigital.nl>
2023-06-16 17:12:32 +0200

    modified:   README.md 	modified:   index.js 	new file:   views/albums.pug 	modified:   views/contacts.pug

--- index.js
+++ index.js
@@ -8,11 +8,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please include the private app access token in your repo BUT only an access token built in a TEST ACCOUNT. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = 'pat-eu1-7d455934-e0dc-4a08-8892-df1eb549c20d';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/albums', async (req, res) => {
+
+    const albums = 'https://api.hubspot.com/crm/v3/objects/2-115195106?properties=name,year';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+
+    try {
+        const resp = await axios.get(albums, { headers });
+        const data = resp.data.results;
+        //res.json(data);
+        res.render('albums', { title: 'Albums | HubSpot APIs', data });      
+    } catch (error) {
+        console.error(error);
+    }
+
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
@@ -20,7 +37,8 @@ const PRIVATE_APP_ACCESS = '';
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+//App.post sample
+
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
